a0a85ff0efeee40a0ee1d85b18c47ffcfc90c3d1
AleSciacca <ale.sciacca25@gmail.com>
2026-06-27 17:16:51 +0200

    Aggiunta pipeline per estrazione feature tramite VideoMAE

--- main.py
+++ main.py
@@ -1,39 +1,95 @@
+import os
 from tqdm import tqdm
-
 import torch
 from torch.utils.data import DataLoader
-
 from src.datasets.video_dataset import VideoKineticsDataset
 from src.models.resnet_baseline import ResNetFeatureExtractor
+from src.models.videomae_extractor import VideoMAEFeatureExtractor
+from transformers import VideoMAEImageProcessor, VideoMAEModel
 
+def main():
+    print("Inizio estrazione feature dai video tramite il modello ResNet18 pre-addestrato su ImageNet...")
+    dataset_resnet = VideoKineticsDataset()
 
-if __name__ == '__main__':
-    print("Inizializzazione del dataset e del modello...")
-    dataset = VideoKineticsDataset()
+    loader_resnet = DataLoader(dataset_resnet, batch_size = 4, shuffle = False)
 
-    kinetics_loader = DataLoader(dataset, batch_size = 4, num_workers = 4)
+    print("Controllo esistenza feature e etichette salvate...")
+    path_features_resnet = 'data/features_resnet18.pt'
+    path_labels_resnet = 'data/labels_resnet18.pt'
 
-    model = ResNetFeatureExtractor()
+    if os.path.exists(path_features_resnet) and os.path.exists(path_labels_resnet):
+        print("Feature e etichette già salvate. Caricamento...")
+        features_list = torch.load(path_features_resnet)
+        labels_list = torch.load(path_labels_resnet)
+        print(f"Feature caricate con forma: {features_list.shape}")
+        print(f"Etichette caricate: {len(labels_list)}")
+    else:
+        print("Feature e etichette non trovate. Estrazione in corso...")
+        model = ResNetFeatureExtractor()
 
-    features_list = []
-    labels_list = []
-    
-    model.eval()
+        features_list = []
+        labels_list = []
 
-    print("Estrazione feature in corso...")
+        model.eval()
 
-    with torch.no_grad():
-        for video, label in tqdm(kinetics_loader):
-            feature = model(video)
-            features_list.append(feature)
-            labels_list.extend(label)
-    
-    print("Salvataggio delle feature e delle etichette...")
-    
-    features_list = torch.cat(features_list, dim=0)
+        print("Estrazione feature in corso...")
 
-    torch.save(features_list, 'features.pt')
-    torch.save(labels_list, 'labels.pt')
+        with torch.no_grad():
+            for video, label in tqdm(loader_resnet):
+                feature = model(video)
+                features_list.append(feature)
+                labels_list.extend(label)
 
-    print(f"Finito! Feature salvate con forma: {features_list.shape}")
-    print(f"Etichette salvate: {len(labels_list)}")
+        print("Salvataggio delle feature e delle etichette...")
+
+        features_list = torch.cat(features_list, dim=0)
+
+        torch.save(features_list, 'data/features_resnet18.pt')
+        torch.save(labels_list, 'data/labels_resnet18.pt')
+
+        print(f"Finito! Feature salvate con forma: {features_list.shape}")
+        print(f"Etichette salvate: {len(labels_list)}")
+
+    print("Inizio estrazione feature dai video tramite il modello VideoMAE...")
+
+    videomae_processor = VideoMAEImageProcessor.from_pretrained("MCG-NJU/videomae-base")
+
+    dataset_videomae = VideoKineticsDataset(transform=videomae_processor)
+    loader_videomae = DataLoader(dataset_videomae, batch_size=4, shuffle=False)
+
+    path_features_videomae = 'data/features_videomae.pt'
+    path_labels_videomae = 'data/labels_videomae.pt'
+
+    if os.path.exists(path_features_videomae) and os.path.exists(path_labels_videomae):
+        print("Feature e etichette già salvate. Caricamento...")
+        features_list_videomae = torch.load(path_features_videomae)
+        labels_list_videomae = torch.load(path_labels_videomae)
+        
+        print(f"Feature caricate con forma: {features_list_videomae.shape}")
+        print(f"Etichette caricate: {len(labels_list_videomae)}")
+    else:
+        print("Feature e etichette non trovate. Estrazione in corso...")
+        model_videomae = VideoMAEFeatureExtractor()
+
+        features_list_videomae = []
+        labels_list_videomae = []
+
+        model_videomae.eval()
+
+        print("Estrazione feature in corso...")
+        with torch.no_grad():
+            for video, label in tqdm(loader_videomae):
+                feature = model_videomae(video)
+                features_list_videomae.append(feature)
+                labels_list_videomae.extend(label)
+
+        print("Salvataggio delle feature e delle etichette...")
+        features_list_videomae = torch.cat(features_list_videomae)
+        torch.save(features_list_videomae, 'data/features_videomae.pt')
+        torch.save(labels_list_videomae, 'data/labels_videomae.pt')
+        
+        print(f"Finito! Feature salvate con forma: {features_list_videomae.shape}")
+        print(f"Etichette salvate: {len(labels_list_videomae)}")
+
+if __name__ == "__main__":
+    main()
